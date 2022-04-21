8a96600a0e753d4b3b9b51efc904fc4d3c8ab8b3
ArchLeaders <archparliament@gmail.com>
2022-04-21 02:14:43 -0700

    Add project files.

---
+++ BotwDecompiler/Formats.cs
@@ -0,0 +1,36 @@
+﻿using BotwScripts.Lib;
+using BotwScripts.Lib.Formats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BotwDecompiler
+{
+    public static class Formats
+    {
+        public static Dictionary<string, List<string>> DB = new();
+        public static void Load()
+        {
+            Mtk.UpdateExternal("formats.sjson", $"{Mtk.GetConfig("dynamic")}\\Data", "BotwScripts.Lib/Data");
+            byte[] bytes = Yaz0.Decompress(Mtk.GetDynamic("formats.sjson"));
+            DB = JsonSerializer.Deserialize<Dictionary<string, List<string>>>(bytes);
+
+            if (DB == null)
+            {
+                CLI.WriteLine("!error||The file formats could not be loaded!");
+                DB = new();
+            }
+        }
+
+        public static string Get(this string file)
+        {
+            FileInfo _file = new(file);
+            var ext = _file.Extension;
+
+            return "";
+        }
+    }
+}
---
+++ BotwDecompiler/Program.cs
@@ -0,0 +1,46 @@
+﻿using BotwScripts.Lib;
+
+var outputDir = ".\\";
+
+if (args.Length < 1)
+    if (!CLI.Option("Not output directory was found. Use the working directory? "))
+        return;
+
+else outputDir = args[0];
+
+Directory.CreateDirectory(outputDir);
+
+var nx = "";
+bool isNx = Mtk.GetConfig("game_dir").Value.GetBoolean();
+
+if (isNx) nx = "_nx";
+
+string? basegame = Mtk.GetConfig($"game_dir{nx}").Value.GetString();
+string? update = Mtk.GetConfig("update_dir").Value.GetString();
+string? dlc = Mtk.GetConfig($"dlc_dir{nx}").Value.GetString();
+
+if (basegame == null)
+{
+    CLI.WriteLine("!error||The base game dump could not be found. Confirm BCML is installed and setup.");
+    return;
+}
+
+if (update == null && isNx != true)
+{
+    CLI.WriteLine("!error||The base game dump could not be found. Confirm BCML is installed and setup.");
+    return;
+}
+
+foreach (var file in Directory.EnumerateFiles(basegame, "*.*", SearchOption.AllDirectories))
+    Decompile(file);
+
+foreach (var file in Directory.EnumerateFiles(basegame, "*.*", SearchOption.AllDirectories))
+    Decompile(file);
+
+foreach (var file in Directory.EnumerateFiles(basegame, "*.*", SearchOption.AllDirectories))
+    Decompile(file);
+
+async Task Decompile(string file)
+{
+
+}
