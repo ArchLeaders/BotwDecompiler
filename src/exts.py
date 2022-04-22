e20a126d3463fe6e741ea7672cdf50cb2e20c82d
ArchLeaders <archparliament@gmail.com>
2022-04-21 20:38:27 -0700

    add extra extensions

---
+++ src/exts.py
@@ -0,0 +1,36 @@
+BARS_EXT = [
+    'bars'
+]
+
+BFEVFL_EXT = [
+    'bfevfl'
+]
+
+BFRES_EXT = [
+    'bfres',
+    'breviewtex',
+    'bitemico',
+    'sbfres',
+    'sbreviewtex',
+    'sbitemico',
+]
+
+HK_EXT = [
+    'hkrb',
+    'hksc',
+    'hknm2',
+    'hktmrb',
+    'hkcl',
+    'hkrg',
+    'shkrb',
+    'shksc',
+    'shknm2',
+    'shktmrb',
+    'shkcl',
+    'shkrg',
+],
+
+MSBT_EXT = [
+    'msbt'
+]
+
