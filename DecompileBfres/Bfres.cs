bbc06c54503bc94590d1e1fe915deb99c4a331ca
ArchLeaders <archparliament@gmail.com>
2022-04-21 23:00:30 -0700

    Bfres loader

---
+++ DecompileBfres/Bfres.cs
@@ -0,0 +1,31 @@
+﻿using Syroot.BinaryData;
+using System.Text;
+
+namespace BfresLibrary
+{
+    /// <summary>
+    /// Copied from KillzXGaming's <a href="https://github.com/KillzXGaming/BfresPlatformConverter/blob/master/Program.cs">BfresPLatformConverter</a>
+    /// </summary>
+    public class Bfres
+    {
+        public static bool IsCompressed(string path)
+        {
+            using (var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (var reader = new BinaryDataReader(fileStream))
+            {
+                string magic = reader.ReadString(4, Encoding.ASCII);
+                return magic == "Yaz0";
+            }
+        }
+
+        public static ResFile LoadBfres(string bfresFile)
+        {
+            if (IsCompressed(bfresFile))
+                using (MemoryStream stream = new(Yaz0.Decompress(bfresFile)))
+                    return new ResFile(stream);
+            else
+                using (MemoryStream stream = new(File.ReadAllBytes(bfresFile)))
+                    return new ResFile(stream);
+        }
+    }
+}
