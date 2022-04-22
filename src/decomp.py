0e8fd0ffc23d42309b151c437f5f2958e3165853
ArchLeaders <archparliament@gmail.com>
2022-04-21 21:32:51 -0700

    evfl decompiler; code cleanup

--- src/decomp.py
+++ src/decomp.py
@@ -3,53 +3,68 @@ import oead
 from pathlib import Path
 from utils import error
 
+
 def aamp(file: Path, out: Path):
     """Decompile an aamp file"""
-    
-    data = file.read_bytes()
 
-    if data[0-4] == b'Yaz0':
-        data = oead.yaz0.decompress(data)
+    try:
+        data = file.read_bytes()
 
-    if data[0-4] != b'AAMP':
-        error(f'[WARNING] Could not decompile \'{file}\' because it was not a valid AAMP file!')
-        return
-        
-    data = oead.aamp.ParameterIO.from_binary(data)
-    data = oead.aamp.ParameterIO.to_text(data)
+        if data[0 - 4] == b"Yaz0":
+            data = oead.yaz0.decompress(data)
 
-    Path(out, '.yml').write_text(data)
+        if data[0 - 4] != b"AAMP":
+            error(
+                f"[WARNING] Could not decompile '{file}' because it was not a valid AAMP file!"
+            )
+            return
+
+        data = oead.aamp.ParameterIO.from_binary(data)
+        data = oead.aamp.ParameterIO.to_text(data)
+
+        Path(out, ".yml").write_text(data)
+
+    except RuntimeError as ex:
+        error(f"[AAMP] {ex}")
+
 
 def bars(file: Path, out: Path):
-    """Decompile an bars file"""
+    """Decompile a bars file"""
 
     from imported.bars_extractor import extract
 
     try:
         extract(file, out)
-    except RuntimeError as re:
-        print(re)
-    
+    except RuntimeError as ex:
+        error(f"[BARS] {ex}")
+
 
 def evfl(file: Path, out: Path):
-    """Decompile an bfevfl file"""
-    
+    """Decompile a bfevfl file"""
+
+    from imported.evfl_to_json import convert
+
+    try:
+        out.write_text(convert(file))
+    except RuntimeError as ex:
+        error(f"[EVFL] {ex}")
+
 
 def bfres(file: Path, out: Path):
-    """Decompile an bfres file"""
-    
+    """Decompile a bfres file"""
+
 
 def byml(file: Path, out: Path):
-    """Decompile an byml file"""
-    
+    """Decompile a byml file"""
+
 
 def havok(file: Path, out: Path):
-    """Decompile an havok file"""
-    
+    """Decompile a havok file"""
+
 
 def msbt(file: Path, out: Path):
-    """Decompile an msbt file"""
+    """Decompile a msbt file"""
 
 
 def sarc(file: Path, out: Path):
-    """Decompile an sarc file"""
+    """Decompile a sarc file"""
